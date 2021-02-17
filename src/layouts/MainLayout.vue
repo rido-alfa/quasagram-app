2cbb4a457f2d7315568dff7c3dc7333ede7f91b1
dodiwkwk <arifaldo0@gmail.com>
2021-02-17 11:20:10 +0700

    button like, comment, share, bookmark, change style navbar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,47 +1,63 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header class="text-grey-10 bg-white" bordered>
-      <q-toolbar class="constrain">
-        <q-btn flat round to="/camera" icon="las la-camera" size="18px" dense class="large-screen-only q-mr-sm" />
-        <q-separator vertical inset spaced class="large-screen-only" />
-        <q-toolbar-title class="text-weight-medium text-grand-hotel"
-          >Quasagram</q-toolbar-title
-        >
-        <q-btn flat round to="/" icon="las la-home" size="18px" dense class="large-screen-only" />
+    <q-layout view="lHh Lpr lFf">
+        <q-header class="text-grey-10 bg-white" bordered>
+			
+            <q-toolbar class="constrain">
+                <q-toolbar-title class="text-weight-medium text-grand-hotel">Quasagram</q-toolbar-title>
+                <q-btn
+                    flat
+                    round
+                    to="/"
+                    icon="las la-home"
+                    size="16px"
+                    dense
+                    class="large-screen-only q-mr-sm"
+                />
+                <q-btn
+                    flat
+                    round
+                    to="/camera"
+                    icon="las la-camera"
+                    size="16px"
+                    dense
+                    class="large-screen-only q-mr-sm q-mr-sm"
+                />
+                <q-item-section avatar>
+                    <q-avatar size="23px">
+                        <img
+                            src="https://avatars.githubusercontent.com/u/46294299?s=460&u=205425f2a3c752dd73448ca8b6b329edb30a948f&v=4"
+                        />
+                    </q-avatar>
+                </q-item-section>
+            </q-toolbar>
+        </q-header>
 
-      </q-toolbar>
-    </q-header>
+        <q-footer class="bg-white small-screen-only" bordered>
+            <q-tabs class="text-grey-10" active-color="grey-5" indicator-color="transparent">
+                <q-route-tab to="/" name="home" icon="las la-home" />
+                <q-route-tab to="/camera" name="camera" icon="las la-camera" />
+            </q-tabs>
+        </q-footer>
 
-    <q-footer class="bg-white small-screen-only" bordered>
-      <q-tabs
-        class="text-grey-10"
-        active-color="grey-5"
-        indicator-color="transparent"
-      >
-        <q-route-tab to="/" name="home" icon="las la-home" />
-        <q-route-tab to="/camera" name="camera" icon="las la-camera" />
-      </q-tabs>
-    </q-footer>
-
-    <q-page-container class="bg-grey-1">
-      <router-view />
-    </q-page-container>
-  </q-layout>
+        <q-page-container class="bg-grey-1">
+            <router-view />
+        </q-page-container>
+    </q-layout>
 </template>
 
 <script>
 export default {
-  name: "MainLayout",
-  data() {
-    return {};
-  }
+    name: "MainLayout",
+    data() {
+        return {};
+    }
 };
 </script>
 
 <style lang="sass">
 .q-toolbar
   @media (min-width: $breakpoint-sm-min)
-    height: 77px
+    height: 54px
 .q-toolbar__title
   font-size: 30px
   @media (max-width: $breakpoint-xs-max)
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,37 +1,105 @@
 <template>
-    <q-page class="constrain q-pa-md">
-        <q-card class="card-post q-mb-md" flat bordered v-for="post in posts" :key="post.id">
-            <q-item>
-                <q-item-section avatar>
-                    <q-avatar>
-                        <img
-                            src="https://avatars.githubusercontent.com/u/46294299?s=460&u=205425f2a3c752dd73448ca8b6b329edb30a948f&v=4"
+    <q-page class="constrain q-py-md q-px-lg home-page">
+        <div class="row q-col-gutter-sm">
+            <div class="col-12 col-sm-8">
+                <q-card
+                    class="card-post q-mb-md"
+                    flat
+                    bordered
+                    v-for="post in posts"
+                    :key="post.id"
+                >
+                    <q-item>
+                        <q-item-section avatar>
+                            <q-avatar>
+                                <img
+                                    src="https://avatars.githubusercontent.com/u/46294299?s=460&u=205425f2a3c752dd73448ca8b6b329edb30a948f&v=4"
+                                />
+                            </q-avatar>
+                        </q-item-section>
+
+                        <q-item-section class="text-grey-10">
+                            <q-item-label class="text-bold">{{ post.username }}</q-item-label>
+                            <q-item-label caption>{{ post.location }}</q-item-label>
+                        </q-item-section>
+
+                        <q-btn
+                            flat
+                            to="/camera"
+                            icon="las la-ellipsis-h"
+                            size="12px"
+                            class="menu-kiri"
                         />
-                    </q-avatar>
-                </q-item-section>
+                    </q-item>
 
-                <q-item-section class="text-grey-10">
-                    <q-item-label class="text-bold"> {{ post.username }} </q-item-label>
-                    <q-item-label caption>{{ post.location }}</q-item-label>
-                </q-item-section>
-            </q-item>
+                    <!-- <q-separator /> -->
+                    <img :src="post.imageUrl" />
 
-            <!-- <q-separator /> -->
-            <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+                    <q-item class="flex justify-between">
+                        <div class="text-grey-10">
+                            <q-btn
+                                flat
+                                dense
+                                class="icon-aksi q-mr-md"
+                                icon="ion-heart-empty"
+                                size="17px"
+                            />
+                            <q-btn
+                                flat
+                                dense
+                                class="icon-aksi q-mr-md"
+                                icon="lar la-comment"
+                                size="17px"
+                            />
+                            <q-btn flat dense class="icon-aksi" icon="ion-paper-plane" size="17px" />
+                        </div>
 
-            <q-card-section class="text-grey-10">
-                <div class="flex items-center text-body2">
-                    <q-item-label class="text-bold q-mr-xs"> {{ post.username }} </q-item-label>
-                    <q-item-label> {{ post.caption }} </q-item-label>
+                        <q-btn
+                            flat
+                            dense
+                            icon="lar la-bookmark"
+                            class="icon-aksi text-grey-10"
+                            size="17px"
+                        />
+                    </q-item>
+
+                    <q-card-section class="text-grey-10 q-py-none">
+                        <div class="flex items-center text-body2">
+                            <q-item-label class="text-bold q-mr-xs">{{ post.username }}</q-item-label>
+                            <q-item-label>{{ post.caption }}</q-item-label>
+                        </div>
+                        <q-item-label
+                            caption
+                            class="q-my-sm text-caption"
+                        >{{ post.date | dateFormat }}</q-item-label>
+                    </q-card-section>
+                </q-card>
+            </div>
+
+            <div class="col-4 large-screen-only">
+                <div class="fixed">
+                    <q-item class="q-mt-lg">
+                        <q-item-section avatar>
+                            <q-avatar size="60px">
+                                <img
+                                    src="https://avatars.githubusercontent.com/u/46294299?s=460&u=205425f2a3c752dd73448ca8b6b329edb30a948f&v=4"
+                                />
+                            </q-avatar>
+                        </q-item-section>
+
+                        <q-item-section class="text-grey-10">
+                            <q-item-label class="text-bold">rido_vcd</q-item-label>
+                            <q-item-label class="text-grey-6">Rido</q-item-label>
+                        </q-item-section>
+                    </q-item>
                 </div>
-                <q-item-label caption class="text-uppercase q-mt-sm text-caption"> {{ post.date }} </q-item-label>
-            </q-card-section>
-        </q-card>
+            </div>
+        </div>
     </q-page>
 </template>
 
 <script>
-import {date} from 'quasar'
+import { date } from "quasar";
 export default {
     name: "PageHome",
     data() {
@@ -63,12 +131,34 @@ export default {
                 }
             ]
         };
+    },
+    filters: {
+        dateFormat(value) {
+            return date.formatDate(value, "MMM YYYY ");
+        }
     }
 };
 </script>
 
 <style lang="sass">
+.home-page
+    @media (max-width: $breakpoint-xs-max)
+        padding: 0
+        // padding-top: 10px
+
 .card-post
-  .q-img
-    min-height: 200px
+    .q-img
+        min-height: 200px
+    @media (max-width: $breakpoint-xs-max)
+        border: none
+        background-color: $grey-1
+
+.menu-kiri
+    .q-btn__wrapper
+        padding: 0
+
+.icon-aksi
+    .q-btn__wrapper
+        padding: 0
+        margin: 0
 </style>
