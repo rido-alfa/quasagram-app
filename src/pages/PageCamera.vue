a3e1582f4b812aa1964b51370039e59885189118
dodiwkwk <arifaldo0@gmail.com>
2021-02-18 13:30:20 +0700

    upload image and disable camera afte take foto and leave from camera page

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -5,7 +5,28 @@
             <canvas v-show="imageCaptured" ref="canvas" class="full-width" height="240" />
         </div>
         <div class="text-center q-pa-md">
-            <q-btn @click="captureImage" unelevated round color="grey-10" icon="ion-camera" />
+            <q-btn
+                v-if="hasCameraSupport"
+                @click="captureImage"
+                unelevated
+                round
+                color="grey-10"
+                icon="ion-camera"
+            />
+            <q-file
+                @input="captureImageFallback"
+                v-if="!hasCameraSupport"
+                color="grey-3"
+                outlined
+                accept="image/*"
+                v-model="imageUpload"
+                label-color="grey-6"
+                label="Upload Photo"
+            >
+                <template v-slot:append>
+                    <q-icon name="attachment" color="grey-6" />
+                </template>
+            </q-file>
         </div>
         <div class="row justify-center q-ma-md">
             <q-input
@@ -34,7 +55,7 @@
 
 <script>
 import { uid } from "quasar";
-require('md-gum-polyfill')
+require("md-gum-polyfill");
 
 export default {
     name: "PageCamera",
@@ -43,44 +64,110 @@ export default {
             post: {
                 id: uid(),
                 caption: "",
-				location: "",
-				photo: null,
-				date: Date.now()
+                location: "",
+                photo: null,
+                date: Date.now()
             },
-            imageCaptured: false
+            imageCaptured: false,
+            imageUpload: [],
+            hasCameraSupport: true
         };
     },
 
     methods: {
         initCamera() {
-            navigator.mediaDevices.getUserMedia({
-                video: true                
-            }).then(stream => {
-                this.$refs.video.srcObject = stream
-            })
+            navigator.mediaDevices
+                .getUserMedia({
+                    video: true
+                })
+                .then(stream => {
+                    this.$refs.video.srcObject = stream;
+                })
+                .catch(error => {
+                    console.log("error", error);
+                    this.hasCameraSupport = false;
+                });
         },
 
         captureImage() {
-            let video = this.$refs.video
-            let canvas = this.$refs.canvas
-            canvas.width = video.getBoundingClientRect().width
-            canvas.height = video.getBoundingClientRect().height
-            let context = canvas.getContext('2d')
-            context.drawImage(video, 0, 0, canvas.width, canvas.height)
-            this.imageCaptured = true
+            let video = this.$refs.video;
+            let canvas = this.$refs.canvas;
+            canvas.width = video.getBoundingClientRect().width;
+            canvas.height = video.getBoundingClientRect().height;
+
+            let context = canvas.getContext("2d");
+            context.drawImage(video, 0, 0, canvas.width, canvas.height);
+            this.imageCaptured = true;
+            this.post.photo = this.dataURItoBlob(canvas.toDataURL());
+            this.disableCamera()
+        },
+
+        captureImageFallback(file) {
+            this.post.photo = file;
+            let canvas = this.$refs.canvas;
+            let context = canvas.getContext("2d");
+
+            let reader = new FileReader();
+            reader.onload = event => {
+                let img = new Image();
+                img.onload = () => {
+                    canvas.width = img.width;
+                    canvas.height = img.height;
+                    context.drawImage(img, 0, 0);
+                    this.imageCaptured = true
+                };
+                img.src = event.target.result;
+            };
+            reader.readAsDataURL(file);
+        },
+
+        disableCamera() {
+            this.$refs.video.srcObject.getVideoTracks().forEach(track => {
+                track.stop()                
+            })
+        },
+
+        dataURItoBlob(dataURI) {
+            // convert base64 to raw binary data held in a string
+            // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+            var byteString = atob(dataURI.split(",")[1]);
+
+            // separate out the mime component
+            var mimeString = dataURI
+                .split(",")[0]
+                .split(":")[1]
+                .split(";")[0];
+
+            // write the bytes of the string to an ArrayBuffer
+            var ab = new ArrayBuffer(byteString.length);
+
+            // create a view into the buffer
+            var ia = new Uint8Array(ab);
+
+            // set the bytes of the buffer to the correct values
+            for (var i = 0; i < byteString.length; i++) {
+                ia[i] = byteString.charCodeAt(i);
+            }
+
+            // write the ArrayBuffer to a blob, and you're done
+            var blob = new Blob([ab], { type: mimeString });
+            return blob;
         }
     },
 
     mounted() {
-        this.initCamera()
+        this.initCamera();
+    },
+    beforeDestroy() {
+        if(this.hasCameraSupport) {
+            this.disableCamera()
+        }
     }
-
-    
 };
 </script>
 
 <style lang="sass">
 .camera-frame
-  border: 1px solid $grey-10
-  border-radius: 5px
+    border: 1px solid $grey-10
+    border-radius: 5px
 </style>
