32f80061cf6fec6b9e00661041001b4786a25d81
dodiwkwk <arifaldo0@gmail.com>
2021-02-18 12:57:08 +0700

    initCamera and captureImage methods

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,10 +1,11 @@
 <template>
     <q-page class="constrain-more q-py-md q-px-lg">
         <div class="q-pa-md camera-frame">
-            <img class="full-width" src="https://cdn.quasar.dev/img/mountains.jpg" />
+            <video v-show="!imageCaptured" ref="video" class="full-width" autoplay />
+            <canvas v-show="imageCaptured" ref="canvas" class="full-width" height="240" />
         </div>
         <div class="text-center q-pa-md">
-            <q-btn unelevated round color="grey-10" icon="ion-camera" />
+            <q-btn @click="captureImage" unelevated round color="grey-10" icon="ion-camera" />
         </div>
         <div class="row justify-center q-ma-md">
             <q-input
@@ -33,6 +34,8 @@
 
 <script>
 import { uid } from "quasar";
+require('md-gum-polyfill')
+
 export default {
     name: "PageCamera",
     data() {
@@ -43,9 +46,36 @@ export default {
 				location: "",
 				photo: null,
 				date: Date.now()
-            }
+            },
+            imageCaptured: false
         };
+    },
+
+    methods: {
+        initCamera() {
+            navigator.mediaDevices.getUserMedia({
+                video: true                
+            }).then(stream => {
+                this.$refs.video.srcObject = stream
+            })
+        },
+
+        captureImage() {
+            let video = this.$refs.video
+            let canvas = this.$refs.canvas
+            canvas.width = video.getBoundingClientRect().width
+            canvas.height = video.getBoundingClientRect().height
+            let context = canvas.getContext('2d')
+            context.drawImage(video, 0, 0, canvas.width, canvas.height)
+            this.imageCaptured = true
+        }
+    },
+
+    mounted() {
+        this.initCamera()
     }
+
+    
 };
 </script>
 
