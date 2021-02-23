2942de9144efefaa761ddbca7654f42ec0ea21f7
dodiwkwk <arifaldo0@gmail.com>
2021-02-23 21:51:23 +0700

    add plugin notify, loading

--- quasar.conf.js
+++ quasar.conf.js
@@ -98,7 +98,7 @@ module.exports = function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: ['Dialog']
+      plugins: ['Dialog', 'Notify', 'Loading']
     },
 
     // animations: 'all', // --- includes all animations
